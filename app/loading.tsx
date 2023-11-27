db534347fdc9c13e40f18b197c33344dfa202c72
Rezaei <ali.rezaei0021@gmail.com>
2023-11-27 15:57:10 +0330

    fix auth error

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { Loader } from "@/components/ui/loader";
-
-
 const Loading = () => {
-  return ( 
+  return (
     <div className="flex h-full w-full items-center justify-center">
-      <Loader />
+      <p>loading ...</p>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Loading;
