42896e198b2337f29deba872e0e784a7380e95d4
Rezaei <ali.rezaei0021@gmail.com>
2023-11-29 16:19:12 +0330

    add zustand

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <Modal onClose={() => {}} title="test" description="test descrip" isOpen>
+        {/* <UserButton afterSignOutUrl="/" /> */}
+        children
+      </Modal>
+     
+    </div>
+  );
 }
