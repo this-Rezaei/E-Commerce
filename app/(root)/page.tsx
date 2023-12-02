b39e369fb475610da81bee1851ea2ab7f9b7ebbb
Rezaei <ali.rezaei0021@gmail.com>
2023-12-02 15:57:07 +0330

    add modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+import { useEffect } from "react";
+import { useStoreModal } from "@/hook/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [onOpen, isOpen]);
+
   return (
     <div className="p-4">
-      <Modal onClose={() => {}} title="test" description="test descrip" isOpen>
-        {/* <UserButton afterSignOutUrl="/" /> */}
-        children
-      </Modal>
-     
+      {/* <Modal onClose={() => {}} title="test" description="test descrip" isOpen> */}
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      {/* /   children */}
+      {/* </Modal> */}
+      test
     </div>
   );
 }
+ 
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,2 +1,36 @@
 "use client";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { useStoreModal } from "@/hook/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+export default function StoreModal() {
+  const storeModal = useStoreModal();
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <Modal
+      title="create store"
+      description="kfgfnhgkfnsdnfdhnfn"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Futur create store
+    </Modal>
+  );
+}
